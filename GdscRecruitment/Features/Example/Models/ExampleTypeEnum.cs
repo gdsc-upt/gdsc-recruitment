eabec77c2b24143fbfaa9f212b153cc1a5e0adb1
Dan Percic <danpercic86@gmail.com>
2022-11-18 00:55:02 +0200

    love enums

--- GdscRecruitment/Features/Example/Models/ExampleTypeEnum.cs
+++ GdscRecruitment/Features/Example/Models/ExampleTypeEnum.cs
@@ -1,8 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using GdscRecruitment.Utilities.Attributes;
+using MudBlazor;
+
 namespace GdscRecruitment.Features.Example.Models;
 
 public enum ExampleTypeEnum
 {
+    [DisplayColor(Color.Info)] [Display(Name = "Easy")]
     EasyExample,
+
+    [DisplayColor(Color.Warning)] [Display(Name = "Hard")]
     NotEasyExample,
+
+    [DisplayColor(Color.Error)] [Display(Name = "WTF")]
     WtfExample
 }
