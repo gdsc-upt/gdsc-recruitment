a2ec29f58ba8661daccf8eed4ca4e57c59cbf6d9
Andrei Dobre <andrei.dobr3@gmail.com>
2023-02-25 13:08:54 +0200

    Updated to .net 7

--- GdscRecruitment/Migrations/20230225105845_Welcome to .NET 7.0.Designer.cs
+++ GdscRecruitment/Migrations/20230225105845_Welcome to .NET 7.0.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using GdscRecruitment.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace GdscRecruitment.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230225105845_Welcome to .NET 7.0")]
+    partial class WelcometoNET70
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ GdscRecruitment/Migrations/20230225105845_Welcome to .NET 7.0.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GdscRecruitment.Migrations
+{
+    /// <inheritdoc />
+    public partial class WelcometoNET70 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
